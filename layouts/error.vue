f3ab89eb82776e0ccb59a1ad7f77c9d4c10ac64c
sean <kailbert@gmail.com>
2022-08-22 16:04:23 +0200

    add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+	.errorpage
+		h1(v-if="error.statusCode === 404") Page not found.
+		h1(v-else) An error occured.
+		HelperButton(to="/") Back to Homepage
+</template>
+
+<script>
+export default {
+	props: ['error'],
+}
+</script>
+
+<style lang="stylus">
+.errorpage
+	min-height 75vh
+	display flex
+	align-items center
+	justify-content center
+	flex-direction column
+</style>
